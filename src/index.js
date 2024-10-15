d4faee32c1d70701fa47fbc252f5f0440907cb13
Tanja <tanja.mskvc@gmail.com>
2024-10-15 11:37:12 +0200

    init

---
+++ src/index.js
@@ -0,0 +1,167 @@
+const audioAssetsPath = "./assets/audio/";
+let score = 0;
+let type = "character";
+
+function play() {
+  $("#audio")[0].play();
+}
+
+const tones = {
+  0: " ",
+  1: "ˉ",
+  2: "ˊ",
+  3: "ˇ",
+  4: "ˋ",
+};
+
+function handleToneInput() {
+  if ($(this).val() in tones) {
+    $(this).val(tones[$(this).val()]);
+    $(this).blur();
+
+    // focus next input
+    let i = $(this).attr("id").split("-").pop();
+    $(`#tone-input-${Number(i) + 1}`).focus();
+  } else {
+    $(this).val("");
+  }
+}
+
+function getToneInputGroup(i) {
+  const toneInputGroup = $(".tone-input-group").first().clone();
+  toneInputGroup.show();
+  toneInputGroup
+    .children(".tone-text-input")
+    .attr({
+      id: `tone-input-${i}`,
+    })
+    .on("input", handleToneInput);
+
+  return toneInputGroup;
+}
+
+function setExpression(expression) {
+  $("#audio").attr("src", audioAssetsPath + expression.filename);
+  $("#tone-input-wrapper").children().slice(1).remove();
+
+  let toneInputGroup;
+  for (let i = 0; i < expression.tones.length; i++) {
+    toneInputGroup = getToneInputGroup(i);
+    $("#tone-input-wrapper").append(toneInputGroup);
+
+    if (i === 0) {
+      toneInputGroup.children("input").first().focus();
+    }
+  }
+}
+
+function getRandomNumber(min, max) {
+  // min and max included
+  return Math.floor(Math.random() * (max - min + 1) + min);
+}
+
+function getExpression(expressionType = "character") {
+  const length = audioData[expressionType].length;
+  const i = getRandomNumber(0, length - 1);
+
+  const filename = audioData[expressionType][i];
+  const tones = filename.split("_")[1].split(".")[0];
+
+  return {
+    filename,
+    tones: tones.split(""),
+  };
+}
+
+let showAnswer = false;
+
+function correct() {
+  showAnswer = true;
+  $("#correct").show();
+
+  score++;
+  $("#score-number").html(score);
+}
+
+function toneNumbersToText(toneNumbers) {
+  return toneNumbers
+    .split("")
+    .map((n) => tones[n])
+    .join("");
+}
+
+function incorrect(correctTones) {
+  showAnswer = true;
+  $("#incorrect").html(toneNumbersToText(correctTones));
+  $("#incorrect").show();
+}
+
+function nextExpression() {
+  showAnswer = false;
+  $("#correct").hide();
+  $("#incorrect").hide();
+
+  let expression = getExpression(type);
+  setExpression(expression);
+}
+
+function getTonesFromFilename(filename) {
+  return filename.split("_").pop().split(".")[0];
+}
+
+function validate() {
+  const toneInputs = $(".tone-text-input:visible");
+  const filename = $("#audio").attr("src");
+  const correctTones = getTonesFromFilename(filename);
+
+  let toneInputValue;
+  for (let i = 0; i < toneInputs.length; i++) {
+    toneInputValue = toneInputs[i].value;
+
+    if (toneInputValue !== tones[correctTones[i]]) {
+      incorrect(correctTones);
+      return;
+    }
+  }
+
+  correct();
+}
+
+$(document).ready(() => {
+  $("#play-button").on("click", () => {
+    play();
+  });
+
+  $(".type-option").on("click", function () {
+    if ($(this).hasClass("type-option-selected")) {
+      return;
+    }
+
+    $(".type-option-selected").removeClass("type-option-selected");
+    $(this).addClass("type-option-selected");
+
+    type = $(this).val();
+    nextExpression();
+  });
+
+  nextExpression();
+});
+
+const shortcuts = {
+  " ": () => {
+    play();
+  },
+  Enter: () => {
+    if (showAnswer) {
+      nextExpression();
+    } else {
+      validate();
+    }
+  },
+};
+
+$(document).on("keydown", (e) => {
+  if (e.key in shortcuts) {
+    shortcuts[e.key]();
+  }
+});
